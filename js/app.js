178fce1f200234c9775db3a529e5ff50e6a4f49f
Mohamed Ali <mohamedali.itc@gmail.com>
2020-08-13 20:41:26 +0200

    add eventlistener to scroll & CSS modifications

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@
 // navbar list (ul)
 const navbarList = document.querySelector('#navbar__list')
 //sections
-const sections = document.querySelectorAll('section')
-
+let sections = document.querySelectorAll('section')
+//links
+// let links = document.querySelectorAll('a')
 
 
 //Loop over the sections : so new sections add to the navbar
@@ -25,6 +26,63 @@ navbarList.appendChild(li)
 li.appendChild(link)
 
 })
+
+
+//SCROLL
+
+//Single section
+// document.addEventListener("scroll",()=>{
+//     //get section
+//     let section = document.querySelector("section")
+//     //use getBoundingClientRect method to get the position of section on the viewport
+//     let rec = section.getBoundingClientRect();
+//     //select anchor tag
+//     let link = document.querySelector("a")
+//     //check if the top of the element less than the height of the viewport using window.scrollY && element bottom is larger than 0
+//     if(rec.top < window.scrollY && rec.bottom >0){
+//         //if true add class active to the anchor tag
+//         link.classList.add("active")
+//     }else{
+//         // if not true remove the class from the anchor tag
+//         link.classList.remove("active")
+//     } 
+// })
+
+document.addEventListener('scroll',()=>{
+    //select all the anchor tags
+    let links = document.querySelectorAll('a')
+    //loop in the NodeList using forEach
+    links.forEach(link=>{
+        //select the section using link.getAttribute to get the href that will be the same as section id
+        let section = document.querySelector(link.getAttribute('href'))
+        //use getBoundingClientRect() to get the element position in the viewport
+        let rec = section.getBoundingClientRect()
+        //make a condition to know if the element in the visible viewport or not 
+        //element top larger than or equal 0 
+        //&& the element bottom is smaller than or equal the viewport innerHeight
+        if(rec.top >= 0 && rec.bottom <= (window.innerHeight) ){
+            //if (true) add class active to the anchor tag
+            link.classList.add('active')
+            console.log(`
+                ${section.getAttribute('id')} 
+                rec top : ${rec.top}
+                rec bottom : ${rec.bottom}
+                window innerHeight : ${window.innerHeight}
+            ` )
+        }else{
+            //if (false) remove the class active
+            link.classList.remove('active')
+        }
+    })
+})
+
+
+
+
+
+    
+
+
 
 
 
